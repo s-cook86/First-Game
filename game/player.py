f6dccd92803786b703bcaf1f50952a71cccec715
Shaun Cook <46054587+s-cook86@users.noreply.github.com>
2018-12-23 14:38:58 +0000

    Add files via upload

---
+++ game/player.py
@@ -0,0 +1,13 @@
+class Player:
+
+    def __init__(self, name, health, weapon):
+        self.name = name
+        self.health = health
+        self.weapon = weapon
+
+    def player(self, name, health, weapon):
+        self.name = name
+        self.health = health
+        self.weapon = weapon
+
+# create a player and assign health points (100)
---
+++ game/weapons.py
@@ -0,0 +1,20 @@
+
+class Weapon:
+
+    def __init__(self, name, attack_strength):
+        self.name = name
+        self.attack_strength = attack_strength
+
+    def sword(self, name, attack_strength):
+        self.name = name
+        self.attack_strength = attack_strength
+
+    def axe(self, name, attack_strength):
+        self.name = name
+        self.attack_strength = attack_strength
+
+    def knife(self, name, attack_strength):
+        self.name = name
+        self.attack_strength = attack_strength
+
+# add in strength characteristics for each weapon
